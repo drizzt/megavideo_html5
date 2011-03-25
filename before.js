7d3ee43cdf870d6cc022e7b4be5f50fb784288cb
Timothy Redaelli <timothy@redaelli.eu>
2011-03-25 23:56:21 +0100

    Use github url

--- before.js
+++ before.js
@@ -7,7 +7,7 @@ function handleBeforeLoadEvent(event)
     {
         document.removeEventListener('beforeload', handleBeforeLoadEvent, true);
         event.target.parentNode.insertBefore(event.target.cloneNode());
-        event.target.src = 'http://videojs.com/video-js/video.js';
+        event.target.src = 'http://drizztbsd.github.com/megavideo_html5/video-js/video.js';
         event.target.charset = 'utf-8';
 
         var css = document.createElement('link');
@@ -15,7 +15,7 @@ function handleBeforeLoadEvent(event)
         css.type = 'text/css';
         css.media = 'screen';
         css.title = 'Video JS';
-        css.href = 'http://videojs.com/video-js/video-js.css';
+        css.href = 'http://drizztbsd.github.com/megavideo_html5/video-js/video-js.css';
         event.target.parentNode.insertBefore(css);
     }
 }
