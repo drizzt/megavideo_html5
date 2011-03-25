cad57f810086dc8efab1ca4c5cea6cfcd36e53ef
Timothy Redaelli <timothy@redaelli.eu>
2011-03-25 13:15:46 +0100

    Slightly optimization and use UNIX file format

--- before.js
+++ before.js
@@ -3,29 +3,25 @@
 
 function handleBeforeLoadEvent(event)
 {
-    if (event.target.nodeName == 'SCRIPT') 
+    if (event.target.nodeName == 'SCRIPT')
     {
-        if (event.target.src == 'http://wwwstatic.megavideo.com/js/swfobject2.js')
-        {
-            var js = document.createElement('script');
-            js.type = 'text/javascript';
-            js.src = event.target.src + '?';
-            event.target.parentNode.appendChild(js);
-            event.target.src = 'http://videojs.com/video-js/video.js';
-            event.target.charset = 'utf-8';
+        document.removeEventListener('beforeload', handleBeforeLoadEvent, true);
+        event.target.parentNode.insertBefore(event.target.cloneNode());
+        event.target.src = 'http://videojs.com/video-js/video.js';
+        event.target.charset = 'utf-8';
 
-            var css = document.createElement('link');
-            css.rel = 'stylesheet';
-            css.type = 'text/css';
-            css.media = 'screen';
-            css.title = 'Video JS';
-            css.href = 'http://videojs.com/video-js/video-js.css';
-            event.target.parentNode.appendChild(css);
-        }
+        var css = document.createElement('link');
+        css.rel = 'stylesheet';
+        css.type = 'text/css';
+        css.media = 'screen';
+        css.title = 'Video JS';
+        css.href = 'http://videojs.com/video-js/video-js.css';
+        event.target.parentNode.insertBefore(css);
     }
 }
 
-(function ()
+
+(function()
 {
     document.addEventListener('beforeload', handleBeforeLoadEvent, true);
 })();
